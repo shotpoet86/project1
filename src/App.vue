bd2902125c3bf09d024f95196b034998a65e72b2
shotpoet86 <curlybracemaster@gmail.com>
2022-04-21 13:54:40 -0400

    newly created vue project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,112 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <body>
+  <header>
+    <h1>Vue Course Goals</h1>
+  </header>
+  <section id="user-goal">
+    <h2>My Course Goal</h2>
+    <p></p>
+  </section>
+  </body>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      myHello: 'Hello'
+    };
   }
-}
+};
 </script>
 
 <style>
-#app {
+app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
-}
+  color: green;
+  }
+
+{
+  box-sizing: border-box
+;
+  }
+
+html {
+  font-family: 'Jost', sans-serif;
+  }
+
+body {
+  margin: 0;
+  }
+
+header {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 3rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  background-color: #4fc08d;
+  color: white;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+  }
+
+#user-goal {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 3rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+  }
+
+#user-goal h2 {
+  font-size: 2rem;
+  border-bottom: 4px solid #ccc;
+  color: #4fc08d;
+  margin: 0 0 1rem 0;
+  }
+
+#user-goal p {
+  font-size: 1.25rem;
+  font-weight: bold;
+  border: 1px solid #4fc08d;
+  background-color: #4fc08d;
+  color: white;
+  padding: 0.5rem;
+  border-radius: 25px;
+  }
+
+#user-goal input {
+  font: inherit;
+  border: 1px solid #ccc;
+  }
+
+#user-goal input:focus {
+  outline: none;
+  border-color: #1b995e;
+  background-color: #d7fdeb;
+  }
+
+#user-goal button {
+  font: inherit;
+  cursor: pointer;
+  border: 1px solid #ff0077;
+  background-color: #ff0077;
+  color: white;
+  padding: 0.05rem 1rem;
+  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
+  }
+
+#user-goal button:hover,
+#user-goal button:active {
+  background-color: #ec3169;
+  border-color: #ec3169;
+  box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+  }
+
+
 </style>
